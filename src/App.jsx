294eb3ca06feafeb37d86baaf3b09977c9c09f51
Alisa Ermel <alisa.ermel@myy.haaga-helia.fi>
2024-10-29 15:11:32 +0200

    Exercise complete

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import CarList from "./components/CarList"
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+
+function App() {
+  return (
+    <Container maxwidth="xl">
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography variant='h6'>Car Shop</Typography>
+        </Toolbar>
+      </AppBar>
+      <CarList />
+      <CssBaseline>
+      </CssBaseline>
+    </Container>
+  )
+}
+
+export default App
